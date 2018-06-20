cf95556836b4f7d553ef956fc2f7d42b98350065
catalvarez <38323797+catalvarez@users.noreply.github.com>
2018-06-19 17:01:54 -0700

    Animated Connectivity Map
    Map structure is working, but not currently called in our new article format

--- public/js/slide4.js
+++ public/js/slide4.js
@@ -1,9 +1,15 @@
 function slide4() {
     console.log("Displaying Slide 4.1");
-    var placeholder = $("<img id='placeholder' width='100%'>");
-    placeholder.attr("src", "slide4_1.png");
-    $("#graphic").append(placeholder);
+    makeConnectivityMap("#graphic", ["INV", "SHA", "FOL","ORO","DNP","ISB","HID"], 
+            {"SHA": ["ORO", "INV"],
+             "ORO": ["INV"],
+             "INV": ["FOL"],
+             "FOL": ["DNP"],
+             "HID": ["DNP"],
+             "ISB": ["DNP"]
+              },connectedResMouseover,connectedResMouseout, "4.1");
 }
+
 
 function slide4_2() {
     console.log("Displaying Slide 4.2");
